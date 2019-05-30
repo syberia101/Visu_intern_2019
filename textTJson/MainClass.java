17e7c8b21fddfa0bee8bc7ee04b0ef234b3698a9
unknown <c1972519@NSAB05ADA3713F5.id.add.cardiff.ac.uk>
2019-05-30 17:48:11 +0100

    Avancement sur le programme text/JSON

--- textTJson/MainClass.java
+++ textTJson/MainClass.java
@@ -6,8 +6,10 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * Created by c1972519 on 5/27/2019.
@@ -24,72 +26,153 @@ public class MainClass {
             //Create CSVWriter object with outputFile as parameter
             //CSVWriter writer = new CSVWriter(tempFile);
 
+            JSONArray arrayOfIpsrc = new JSONArray();
+            JSONArray arrayOfIpDst = new JSONArray();
+            JSONArray arrayOfDestPort = new JSONArray();
+
             try {
                 String line;
-                String line1;
-
-                line1 = buff.readLine();
-                int positionSrc1 = line1.indexOf("src=");
-                int positionDst1 = line1.indexOf("dst=");
-                String ipsrc1 = line1.substring(positionSrc1 + 4, positionDst1 - 1);
+                String oldIpdest = new String("0");
+                String oldPort =  new String("0");
 
                 while ((line = buff.readLine()) != null) {
-                    int positionSrc = line.indexOf("src=");
-                    int positionDst = line.indexOf("dst=");
-                    int positionSrcPort = line.indexOf("src_port=");
-                    int positionDstPort = line.indexOf("dst_port=");
-                    int positionSrcXlated = line.indexOf("src-xlated");
+                    String newIpsrc = findPositionSrc(line);
+                    String newIpdest = findPositionDest(line);
+                    String newPort = findPositionDest(line);
 
-                    String ipsrc = line.substring(positionSrc + 4, positionDst - 1);
-                    String ipdst = line.substring(positionDst + 4, positionSrcPort - 1);
-                    String destPort = line.substring(positionDstPort + 9, positionSrcXlated - 1);
+                    if(newIpdest.equals(oldIpdest)){
+                        if(newPort.equals(oldPort)){
+                            arrayOfIpsrc = addIpSrc(arrayOfIpsrc, createNewIpSrc(newIpsrc));
+                        }else{
+                            //JSONArray otherArrayOfDestPort = new JSONArray();
+                            //JSONArray otherArrayOfIpsrc = new JSONArray();
+                            arrayOfIpsrc = addIpSrc(arrayOfIpsrc, createNewIpSrc(newIpsrc));
+                            arrayOfDestPort = addPort(arrayOfDestPort, createNewPort(newPort));
+                        }
+                    }else{
+                        //JSONArray otherArrayOfIpsrc = new JSONArray();
+                        arrayOfIpDst = addIpDest(arrayOfIpDst, newIpdest);
 
-                    //Add data to CSV
-                    /*String[] data = {ipsrc, ipdst, destPort};
-                    String oui = "\r\n";
-                    String[] space = {oui};
-                    writer.writeNext(data);
-                    writer.writeNext(space);*/
-
-                    JSONObject ipsrcUp = new JSONObject();
-                    ipsrcUp.put("ipsrc", ipsrc);
-
-                    JSONArray arrayOfIpsrc = new JSONArray();
-                    arrayOfIpsrc.add(ipsrcUp);
-
-                    JSONObject destPortUp = new JSONObject();
-                    destPortUp.put("port", destPort);
-
-                    JSONArray arrayOfDestPort = new JSONArray();
-                    arrayOfDestPort.add(destPortUp);
-
-                    JSONObject ipdstUp = new JSONObject();
-                    ipdstUp.put("ipdst", ipdst);
-
-                    JSONArray arrayOfIpDst = new JSONArray();
-                    arrayOfIpDst.add(ipdstUp);
-
-                    JSONObject firstLine = new JSONObject();
-                    firstLine.put("IPDEST", "null");
-                    firstLine.put("children", arrayOfIpDst);
-
-                    /*if(ipsrc1 == ipsrc){
-
-                    }*/
-                    try (FileWriter writer = new FileWriter(output)) {
-
-                        writer.write(ipsrcUp.toJSONString());
-                        writer.flush();
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
+
+                    oldIpdest = newIpdest;
+                    oldPort = newPort;
                 }
             }finally{
                 buff.close();
+
+                try (FileWriter writer = new FileWriter(output)) {
+
+                    writer.write(createFirstLine(arrayOfIpDst).toJSONString());
+                    writer.flush();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    /////////////// Trouver la position des elements ////////////////
+    static String findPositionSrc(String line){
+        int positionSrc = line.indexOf("src=");
+        int positionDst = line.indexOf("dst=");
+
+        String ipsrc = line.substring(positionSrc + 4, positionDst - 1);
+        return ipsrc;
+    }
+
+    static String findPositionDest(String line){
+        int positionDst = line.indexOf("dst=");
+        int positionSrcPort = line.indexOf("src_port=");
+
+        String ipdst = line.substring(positionDst + 4, positionSrcPort - 1);
+        return ipdst;
+    }
+
+    static String findPositionPort(String line){
+        int positionDstPort = line.indexOf("dst_port=");
+        int positionSrcXlated = line.indexOf("src-xlated");
+
+        String destPort = line.substring(positionDstPort + 9, positionSrcXlated - 1);
+        return destPort;
+    }
+
+    ////////////////// Creer nouvel objet JSON ////////////////
+    public static JSONObject createNewIpSrc(String ipsrc){
+        JSONObject ipsrcUp = new JSONObject();
+        ipsrcUp.put("ipsrc", ipsrc);
+
+        return ipsrcUp;
+    }
+
+    public static String createNewIpDest(String ipdst){
+        /*JSONObject ipdstUp = new JSONObject();
+        ipdstUp.put("ipdst", ipdst);*/
+
+        return ipdst;
+    }
+
+    public static JSONObject createNewPort(String destPort){
+        JSONObject destPortUp = new JSONObject();
+        destPortUp.put("port", destPort);
+
+        return destPortUp;
+    }
+
+    ///////////////// Ajouter JSON Object a la liste /////////////////
+    public static JSONArray addIpSrc(JSONArray arrayOfIpsrc, JSONObject ipsrcUp){
+        arrayOfIpsrc.add(ipsrcUp);
+
+        return arrayOfIpsrc;
+    }
+
+    public static JSONArray addIpDest(JSONArray arrayOfIpDst, JSONObject ipdstUp){
+        arrayOfIpDst.add(ipdstUp);
+
+        return arrayOfIpDst;
+    }
+
+    public static JSONArray addPort(JSONArray arrayOfDestPort, JSONObject destPortUp){
+        arrayOfDestPort.add(destPortUp);
+
+        return arrayOfDestPort;
+    }
+
+    /////////////////// Creer surobjet ////////////////////
+    public static JSONObject createPortDest(String port, JSONArray arrayForPort){
+        JSONObject objPort = new JSONObject();
+        objPort.put("port", port);
+        objPort.put("children", arrayForPort);
+
+        return objPort;
+    }
+
+    public static JSONObject createIpDest(ArrayList arrayOfIpdst, JSONArray arrayForIpDst){
+        for(int i=1; i<arrayOfIpdst.size(); i++) {
+            String ipdst = new String(arrayOfIpdst.get(i).toString());
+
+            JSONObject objIpDst = new JSONObject();
+            objIpDst.put("ipdst", ipdst);
+            objIpDst.put("children", arrayForIpDst);
+
+            return objIpDst;
+        }
+    }
+
+    public static JSONObject createFirstLine(JSONArray arrayOfIpDst){
+        JSONObject firstLine = new JSONObject();
+        firstLine.put("IPDEST", "null");
+        firstLine.put("children", arrayOfIpDst);
+
+        return firstLine;
+    }
 }
+
+//Add data to CSV
+/*String[] data = {ipsrc, ipdst, destPort};
+String oui = "\r\n";
+String[] space = {oui};
+writer.writeNext(data);
+writer.writeNext(space);*/
